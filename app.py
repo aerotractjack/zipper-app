80adf40b8a08ffb43e5d78dc40ca56366da7b9d0
jack <jackw@aerotractone.com>
2023-07-26 10:48:30 -0700

    backup

--- app.py
+++ app.py
@@ -41,32 +41,91 @@ separators.
 app = Flask(__name__, template_folder="templates")
 app.secret_key = secret.secret_key
 
+class DirZipper:
+
+    def __init__(self, base_dir):
+        if base_dir[:2] == "Z:":
+            base_dir = base_dir.replace(
+                r'Z:\Clients', r'/home/aerotract/NAS/main/Clients').replace('\\', '/')
+        self.base_dir = Path(base_dir).as_posix()
+
+    def collect_filenames(self):
+        files = os.listdir(self.base_dir)
+        file_names = [f.split(".")[0] for f in files]
+        collected_filenames = []
+        for f in file_names:
+            if not os.path.exists(f + ".shp"):
+                continue
+            collected_filenames.append(f)
+        return collected_filenames
+
+    def zip_files(self, filenames):
+        zipped = []
+        for f in filenames:
+            zipped.append(self.zip_from_filename(f))
+        return zipped
+
+    def zip_from_filename(self, filename):
+        with tempfile.TemporaryDirectory() as tempdir:
+            zipfname_temp = f"{tempdir}/{filename}.zip"
+            zipf = zipfile.ZipFile(zipfname_temp, 'w', zipfile.ZIP_DEFLATED)
+            os.chdir(self.base_dir)
+            for file in glob.glob(f"{filename}.*"):
+                zipf.write(file)
+            zipf.close()
+            zipfname_final = f"{self.base_dir}/{filename}.zip"
+            shutil.move(zipfname_temp, zipfname_final)
+        return zipfname_final
+
+    @classmethod
+    def zip_dir(cls, base_dir):
+        z = cls.__init__(base_dir)
+        filenames = z.collect_filenames()
+        zipped = z.zip_files(filenames)
+        return (zipped, len(zipped))
+
+# @app.route('/', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         base_dir = request.form.get('base_dir')
+#         if base_dir[:2] == "Z:":
+#             base_dir = base_dir.replace(
+#                 r'Z:\Clients', r'/home/aerotract/NAS/main/Clients').replace('\\', '/')
+#         base_dir = Path(base_dir).as_posix()
+#         files = os.listdir(base_dir)
+#         files = [f.split(".")[0] for f in files]
+#         counter = Counter(files)
+#         to_zip = [f for f, c in counter.items() if c > 0]
+#         zipped = []
+#         for zf in to_zip:
+#             with tempfile.TemporaryDirectory() as tempdir:
+#                 zipfname_temp = f'{tempdir}/{zf}.zip'
+#                 zipf = zipfile.ZipFile(zipfname_temp, 'w', zipfile.ZIP_DEFLATED)
+#                 os.chdir(base_dir)
+#                 for file in glob.glob(f"{zf}.*"):
+#                     zipf.write(file)
+#                 zipf.close()
+#                 zipfname_final = f'{base_dir}/{zf}.zip'
+#                 shutil.move(zipfname_temp, zipfname_final)
+#             zipped.append(zipfname_final)
+#             session["zipped"] = zipped
+#             session["count"] = len(zipped)
+#         sys.stdout.flush()
+#         print("=================")
+#         print("Base dir: ", base_dir)
+#         print("Projects zipped: ", len(zipped))
+#         print("=================")
+#         sys.stdout.flush()
+#         return redirect("/success")
+#     return render_template("home.html")
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         base_dir = request.form.get('base_dir')
-        if base_dir[:2] == "Z:":
-            base_dir = base_dir.replace(
-                r'Z:\Clients', r'/home/aerotract/NAS/main/Clients').replace('\\', '/')
-        base_dir = Path(base_dir).as_posix()
-        files = os.listdir(base_dir)
-        files = [f.split(".")[0] for f in files]
-        counter = Counter(files)
-        to_zip = [f for f, c in counter.items() if c == 4]
-        zipped = []
-        for zf in to_zip:
-            with tempfile.TemporaryDirectory() as tempdir:
-                zipfname_temp = f'{tempdir}/{zf}.zip'
-                zipf = zipfile.ZipFile(zipfname_temp, 'w', zipfile.ZIP_DEFLATED)
-                os.chdir(base_dir)
-                for file in glob.glob(f"{zf}.*"):
-                    zipf.write(file)
-                zipf.close()
-                zipfname_final = f'{base_dir}/{zf}.zip'
-                shutil.move(zipfname_temp, zipfname_final)
-            zipped.append(zipfname_final)
-            session["zipped"] = zipped
-            session["count"] = len(zipped)
+        zipped, count = DirZipper.zip_dir(base_dir)
+        session["zipped"] = zipped
+        session["count"] = count
         sys.stdout.flush()
         print("=================")
         print("Base dir: ", base_dir)
